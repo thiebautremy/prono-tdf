3465787c12cdee05af5835f8e432ec31405bae3f
thiebautremy <remythiebaut@msn.com>
2022-10-30 18:01:56 +0100

    formated date timestamp firestore

--- src/Components/Admin/Stages/Stage/Stage.tsx
+++ src/Components/Admin/Stages/Stage/Stage.tsx
@@ -6,19 +6,36 @@ interface Props {
     stageId: number;
     startCity: string;
     endCity: string;
-    date: string;
+    date: { seconds: number; nanoseconds: number };
     lengthStage: number;
     type: string;
   };
 }
 const Stages: React.FC<Props> = ({ stage }) => {
-  const timeObj = new Timestamp(stage.date.seconds, stage.date.nanoseconds);
-  console.log(timeObj instanceof Timestamp);
-  console.log(timeObj.toDate());
+  //TODO Faire une fonction générique pour la date en retournant un objet avec la date formatée et l'heure {date, time}
+  const getDateFormated = (timestampFirestore: {
+    seconds: number;
+    nanoseconds: number;
+  }) => {
+    const timeObj = new Timestamp(
+      timestampFirestore.seconds,
+      timestampFirestore.nanoseconds
+    );
+    const dateAndHour = timeObj.toDate();
+    const minutes =
+      dateAndHour.getMinutes() < 10
+        ? `0${dateAndHour.getMinutes()}`
+        : `${dateAndHour.getMinutes()}`;
+    return `${dateAndHour.getDate()}/${
+      dateAndHour.getMonth() + 1
+    }/${dateAndHour.getUTCFullYear()} à ${dateAndHour.getHours()}:${minutes}`;
+  };
+
+  console.log(getDateFormated(stage.date));
   return (
     <div className="stage">
       <h1>Etape n° {stage.stageId}</h1>
-      {/* <h1>{stage.date}</h1> */}
+      <h1>{getDateFormated(stage.date)}</h1>
       <h1>{`${stage.startCity} => ${stage.endCity}`}</h1>
       <h2>Type: {stage.type}</h2>
       <h2>{stage.lengthStage} km</h2>

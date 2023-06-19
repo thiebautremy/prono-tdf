ca6f58776f67b70c1d330a0e933c5aab1494039b
thiebautremy <remythiebaut@msn.com>
2023-06-19 14:54:15 +0200

    add table awardedpoints

--- src/Components/Resultats/Resultats.tsx
+++ src/Components/Resultats/Resultats.tsx
@@ -12,6 +12,7 @@ import { UserConnectedInfo } from "../../Context/userContext";
 import Resultat from "./Resultat";
 import "./Resultats.scss";
 import map from "../../assets/pictures/f8d5d.jpg";
+import { awardedPoints } from "../../assets/points/points";
 
 const Resultats = () => {
   const [users, setUsers] = useState<DocumentData>([]);
@@ -35,10 +36,11 @@ const Resultats = () => {
   useEffect(() => {
     fetchUsers();
   }, []);
+  console.log(Object.values(awardedPoints));
   return (
     <div className="resultats">
       <div className="resultats__fixture">
-        <p>Classement</p>
+        <h1>Classement</h1>
       </div>
       <div className="resultats__scores">
         {users.length > 0 &&
@@ -46,7 +48,26 @@ const Resultats = () => {
             <Resultat key={user.authId} {...user} />
           ))}
       </div>
-      <div className="map"></div>
+      <h2>Points attribués en fonction du classement du coureur</h2>
+      <table className="resultats__table">
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Points attribués</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(awardedPoints).map((awardedPoint, index) => (
+            <tr key={index}>
+              <td>
+                {`${index + 1}`}
+                <sup>{`${index === 0 ? " er" : " ième"}`}</sup>
+              </td>
+              <td>{`${awardedPoint} pts`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <img src={map} alt="" className="map" />
     </div>
   );
